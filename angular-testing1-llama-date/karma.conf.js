ad35c7856962718d058d2b62b5f98923a4cd40ec
Emmanuel De Saint Steban <emmanueldesaintsteban@gmail.com>
2019-11-20 13:36:47 +0000

    pass to step two

--- angular-testing1-llama-date/karma.conf.js
+++ angular-testing1-llama-date/karma.conf.js
@@ -7,11 +7,11 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
+      require('karma-mocha-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
-
+    
     // TRIGGER
     autoWatch: true,
     singleRun: false,
@@ -29,11 +29,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
 
-    reporters: ['progress', 'kjhtml'],
-
-    client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
-    },
+    reporters: ['mocha'],
 
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, './coverage/llama-date'),
